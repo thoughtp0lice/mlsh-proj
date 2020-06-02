6eb38d56549b0e50d26de98aabced2f063ea8785
Human McPerson <humanmcperson@Humans-MacBook-Pro.local>
2020-06-01 17:24:33 -0700

    movement4

--- code/test.py
+++ code/test.py
@@ -2,12 +2,19 @@ import test_envs
 import gym
 import numpy as np
 import mlsh_util
-import policy
-import pickle
 
-agent = pickle.load(open("agent.p","rb"))
-env = gym.make("MovementBandits-v0")
-print(env.render(mode='rgb_array'))
+env = gym.make("MovementBandits4-v0")
 total_reward = 0
-print(agent.rollout_render(env, 50, 10))
+for i in range(100):
+    goals = env.env.goals
+    obs = env.reset()
+    for k in range(50):
+        env.render()
+        obs, reward, done, _ = env.step(np.random.choice(5, 1))
+        total_reward += reward
+    if i % 10 == 0:
+        print(obs)
+
+        
+print(total_reward)
 env.close()
---
+++ test_envs/test_envs/__init__.py
@@ -0,0 +1,36 @@
+import logging
+from gym.envs.registration import register
+
+logger = logging.getLogger(__name__)
+
+register(
+    id='MovementBandits-v0',
+    entry_point='test_envs.envs:MovementBandits',
+    timestep_limit=50,
+)
+
+register(
+    id='MovementBandits4-v0',
+    entry_point='test_envs.envs:MovementBandits4',
+    timestep_limit=50,
+)
+
+register(
+    id='KeyDoor-v0',
+    entry_point='test_envs.envs:KeyDoor',
+    timestep_limit=100,
+)
+
+register(
+    id='Allwalk-v0',
+    entry_point='test_envs.envs:Allwalk',
+    timestep_limit=50,
+)
+
+register(
+    id='Fourrooms-v0',
+    entry_point='test_envs.envs:Fourrooms',
+    timestep_limit=100,
+    reward_threshold = 1,
+)
+
